f6368ba5787c60bd82700a14381e43c99360d726
varun0209 <varun7654321@gmail.com>
2020-03-24 16:31:25 +0530

    bug fixes

--- src/app/reuse-components/index.ts
+++ src/app/reuse-components/index.ts
@@ -4,3 +4,4 @@ export { DeleteItemComponent } from './delete-item/delete-item.component';
 export { SearchFilterTableComponent } from './search-filter-table/search-filter-table.component'
 export { ReportTableComponent } from './report-table/report-table.component';
 export { ReportsInnerTableComponent } from './reports-inner-table/reports-inner-table.component';
+export { PrintComponent } from './print/print.component';
